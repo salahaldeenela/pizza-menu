db68ed26256633f8fda80ccf6c5ac7b14f8bc16b
salahaldeenel <109271967+salahaldeenel@users.noreply.github.com>
2024-10-16 21:24:24 +0300

    Simple push

--- public/data.js
+++ public/data.js
@@ -1,5 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 const pizzaData = [
   {
     name: "Focaccia",
@@ -44,31 +42,3 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
-function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
-  );
-}
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Spinaci pizza"></img>
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato,mozarella,spinach,and ricotta cheese</p>
-    </div>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
